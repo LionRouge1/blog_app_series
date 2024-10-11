d1bbf928e7e14c76c11f7c6eeb4a60a9e17d2a20
LionRouge1 <matchoudiavlessi@gmail.com>
2024-10-11 11:35:25 +0000

    Fix bugs

--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class SignUpView(CreateView):
   form_class = CustomUserCreationForm
   model = User
-  success_url = reverse_lazy('/')
+  success_url = reverse_lazy('home')
   template_name = 'registration/signup.html'
