21f4b925f28025f3fddf40718a69cd15c880c434
LionRouge1 <matchoudiavlessi@gmail.com>
2024-10-11 09:32:21 +0000

    Remove comments

--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from forms import CustomUserCreationForm
 
-# Create your views here.
 class SignUpView(CreateView):
   form_class = CustomUserCreationForm
   success_url = reverse_lazy('login')
