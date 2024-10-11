99ae25318c2562d35f675bbc53b5beb6f3021cab
LionRouge1 <matchoudiavlessi@gmail.com>
2024-10-11 09:27:35 +0000

    Add test with empty field and make sure tests are failing

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -4,26 +4,42 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class SignUpPageTests(TestCase):
-    def setUp(self) -> None:
-        self.name = 'test user'
-        self.email = 'testuser@email.com'
-        self.bio = 'test user bio'
-        self.password = 'fake12345'
+  def setUp(self) -> None:
+    self.full_name = 'test user'
+    self.email = 'testuser@email.com'
+    self.bio = 'test user bio'
+    self.password = 'fake12345'
 
-    def test_signup_page_view(self):
-        response = self.client.get(reverse('users:signup'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, template_name='registration/signup.html')
+  def test_signup_page_view(self):
+    """The Signup url should render the 'registration/signup.html' template"""
+    response = self.client.get(reverse('users:signup'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, template_name='registration/signup.html')
 
-    def test_signup_form(self):
-        response = self.client.post(reverse('users:signup'), data={
-            'name': self.name,
-            'email': self.email,
-            'bio': self.bio,
-            'password1': self.password,
-            'password2': self.password
-        })
+  def test_signup_correct_data(self):
+    """User should be saved when a correct data is provided"""
+    response = self.client.post(reverse('users:signup'), data={
+      'name': self.full_name,
+      'email': self.email,
+      'bio': self.bio,
+      'password1': self.password,
+      'password2': self.password
+    })
 
-        users = User.objects.all()
-        self.assertEqual(users.count(), 1)
-        self.assertNotEqual(users[0].password, self.password)
+    users = User.objects.all()
+    self.assertEqual(users.count(), 1)
+    self.assertNotEqual(users[0].password, self.password)
+
+  def test_signup_fake_data(self):
+    """User shouldn't be save we missing email field"""
+    response = self.client.post(reverse('users:signup'), data={
+      'name': self.full_name,
+      'email': '',
+      'bio': self.bio,
+      'password1': self.password,
+      'password2': self.password
+    })
+    
+    self.assertEqual(response.status_code, 200)
+    users = User.objects.all()
+    self.assertEqual(users.count(), 0)
