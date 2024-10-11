b0ab3a01139ba820184c88acd541b21f4ca5feb5
LionRouge1 <matchoudiavlessi@gmail.com>
2024-10-11 08:52:38 +0000

    Add the media url to the project url

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings # new line
+from django.conf.urls.static import static # new line
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
