186a13927116ac82f1b8d36d544bd238704e2f8b
LionRouge1 <matchoudiavlessi@gmail.com>
2024-10-11 11:32:50 +0000

    Add users app urls to the project

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings # new line
 from django.conf.urls.static import static # new line
@@ -24,6 +24,7 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
