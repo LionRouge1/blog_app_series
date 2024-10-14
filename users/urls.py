13052f7d32d7412f9c500374f53c3a748a5cf117
LionRouge1 <matchoudiavlessi@gmail.com>
2024-10-14 07:32:30 +0000

    Create a path for the CustomLoginView and LogoutView

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
+  path('log_in/', views.CustomLoginView.as_view(), name='login' ),
   path('sign_up/', views.SignUpView.as_view(), name='signup'),
+  path('log_out/', auth_views.LogoutView.as_view(), name='logout'),
 ]
